b254d4aeefa27e8734afc575d6b8c4d1647f04a9
emilien <emilien@feliseed.co.jp>
2023-04-22 19:53:55 +0900

    setup dummy login

--- src/routes/login/+server.ts
+++ src/routes/login/+server.ts
@@ -1,5 +1,11 @@
 import { type RequestHandler, redirect } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async () => {
+    console.log('Dummy Login Detected');
+    throw redirect(302,'/app/dashboard');
+};
+
+export const POST: RequestHandler = async (request) => {
+    console.log('Dummy POST Login Detected', request);
     throw redirect(302,'/app/dashboard');
 };
