3209291fc019bb041e3481046180f99bdba8a866
emilien <emilien@feliseed.co.jp>
2023-05-04 04:04:02 +0900

    Dictionary - basic init, UI setup

---
+++ src/routes/app/dictionary/+page.server.js
@@ -0,0 +1,17 @@
+// @ts-nocheck
+
+import { message, superValidate } from 'sveltekit-superforms/server';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({locals: { supabase, getSession}}) {
+
+    const { user } = await getSession();
+
+    let { data: vocabData, error } = await supabase.from('user_vocabulary')
+                                               .select('vocabulary_id, vocabulary(*), custom_translation')
+                                               .eq('user_id', user.id)
+
+    console.log(vocabData);
+
+    return { vocabData };
+}
--- src/routes/app/dictionary/+page.svelte
+++ src/routes/app/dictionary/+page.svelte
@@ -1,5 +1,36 @@
 <script lang="ts">
-    import NavButton from "$lib/components/atoms/NavButton.svelte";
-    import Sidebar from "$lib/components/organisms/Sidebar.svelte";
+    import UnderConstruction from '$lib/components/atoms/UnderConstruction.svelte';
+    import type { PageData } from './$types';
+    import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
+
+    export let data: PageData;
+
+    console.log(data);
 </script>
+
+<UnderConstruction />
+
+<section class="mt-12 p-2">
+    <Table striped={true}>
+        <TableHead>
+            <TableHeadCell>English</TableHeadCell>
+            <TableHeadCell>日本語（既定）</TableHeadCell>
+            <TableHeadCell>日本語<br/>（カスタム）</TableHeadCell>
+        </TableHead>
+        <TableBody>
+            {#each data.vocabData as item, key}
+                <TableBodyRow>
+                    <TableBodyCell>{item.vocabulary.en_word}</TableBodyCell>
+                    <TableBodyCell>{item.vocabulary.jp_word ?? '-'}</TableBodyCell>
+                    <TableBodyCell>{item.custom_translation ?? '-'}</TableBodyCell>
+                </TableBodyRow>
+            {/each}
+            <TableBodyRow>
+
+            </TableBodyRow>
+        </TableBody>
+    </Table>
+</section>
+
+
 
