1909ccf9f6a627c0637bd27d05fe0385b91d9c41
emilien <emilien@feliseed.co.jp>
2023-05-03 12:08:11 +0900

    settings / dashboard - cleanup

--- src/routes/app/dashboard/+page.svelte
+++ src/routes/app/dashboard/+page.svelte
@@ -2,109 +2,11 @@
 import { onMount } from "svelte";
 import NavButton from "$lib/components/atoms/NavButton.svelte";
 import Sidebar from "$lib/components/organisms/Sidebar.svelte";
+import type { PageData } from "./$types";
 
-let tableData = Array.from({ length: 5 }, () => Array(5).fill(""));
-let tableRef: any;
-let selectedCells: any;
-let mouseDown = false;
-let startCell, endCell;
+export let data: PageData;
 
-onMount(() => {
-    tableRef.addEventListener("paste", handlePaste);
-    tableRef.addEventListener('mousedown', handleMouseDown);
-    tableRef.addEventListener('mouseup', handleMouseUp);
-    tableRef.addEventListener('mouseover', handleMouseOver);
-    tableRef.addEventListener('paste', handlePaste);  
+console.log(data);
 
-    return () => {
-      tableRef.removeEventListener('mousedown', handleMouseDown);
-      tableRef.removeEventListener('mouseup', handleMouseUp);
-      tableRef.removeEventListener('mouseover', handleMouseOver);
-      tableRef.removeEventListener('paste', handlePaste);
-    };
-});
-
-
-
-function handlePaste(e) {
-    e.preventDefault();
-    const clipboardData = e.clipboardData.getData("text");
-    const rows = clipboardData.split("\n").map((row) => row.split("\t"));
-    for (let i = 0; i < Math.min(rows.length, tableData.length); i++) {
-      for (let j = 0; j < Math.min(rows[i].length, tableData[i].length); j++) {
-      tableData[i][j] = rows[i][j];
-    }
-}
-}
-
-function handleMouseDown(e) {
-    if (e.target.tagName === 'TD') {
-      mouseDown = true;
-      startCell = e.target;
-      selectedCells.clear();
-      selectedCells.add(startCell);
-      updateSelection();
-    }
-  }
-
-  function handleMouseUp(e) {
-    mouseDown = false;
-  }
-
-  function handleMouseOver(e) {
-    if (mouseDown && e.target.tagName === 'TD') {
-      endCell = e.target;
-      updateSelection();
-    }
-  }
-
-  function updateSelection() {
-    selectedCells.forEach(cell => cell.classList.remove('selected'));
-    selectedCells.clear();
-
-    const startRow = startCell.parentNode.rowIndex;
-    const startCol = startCell.cellIndex;
-    const endRow = endCell.parentNode.rowIndex;
-    const endCol = endCell.cellIndex;
-
-    const minRow = Math.min(startRow, endRow);
-    const maxRow = Math.max(startRow, endRow);
-    const minCol = Math.min(startCol, endCol);
-    const maxCol = Math.max(startCol, endCol);
-
-    for (let i = minRow; i <= maxRow; i++) {
-      for (let j = minCol; j <= maxCol; j++) {
-        const cell = tableRef.rows[i].cells[j];
-        cell.classList.add('selected');
-        selectedCells.add(cell);
-      }
-    }
-  }
 </script>
 
-<div id="editor" />
-
-<div>
-  <table bind:this={tableRef}>
-    {#each tableData as row, i}
-      <tr>
-        {#each row as cell, j}
-          <td contenteditable>{cell}</td>
-        {/each}
-      </tr>
-    {/each}
-  </table>
-</div>
-
-<style>
-  table {
-    border-collapse: collapse;
-    margin: 20px;
-  }
-  th,
-  td {
-    border: 1px solid #ccc;
-    padding: 8px;
-    text-align: left;
-  }
-</style>
--- src/routes/app/settings/+layout.svelte
+++ src/routes/app/settings/+layout.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
     import NavButton from '$lib/components/atoms/NavButton.svelte';
     import { Scaffolder } from '/src/config/scaffolder';
-
+    import { TabGroup, Tab } from '@skeletonlabs/skeleton';
     let subMenus = Scaffolder.OptionsNav;
+
+    let tabSet: number = 0;
 </script>
 
-<nav id="options-header" class="flex flex-row">
-    {#each subMenus as menu}
-        <NavButton  />
-    {/each}
-</nav>
+<TabGroup>
+    <Tab bind:group={tabSet} name="tab1" value={0}>Vocab List Setting</Tab>
+    <Tab bind:group={tabSet} name="tab2" value={1}>Reading Settings</Tab>
+    <Tab bind:group={tabSet} name="tab3" value={2}>Advanced</Tab>
+</TabGroup>
 
 <div>
     <slot></slot>
