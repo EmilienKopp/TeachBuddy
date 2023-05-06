103a368ae628608dcf2a6ee5a72a435a84099a01
emilien <emilien@feliseed.co.jp>
2023-05-07 03:17:01 +0900

    Defined dummy data

---
+++ src/config/constants.js
@@ -0,0 +1,10 @@
+export const DUMMY_PASSAGE =  
+ `        Having friends is really important. Friends are people who are nice to us and make us feel happy. They like to play with us, share things with us, and help us when we need it. 
+
+Having friends can be really helpful. Sometimes we feel sad or scared, and having a friend to talk to can make us feel better. And if we have trouble with something we’re doing, a friend can help us figure it out.
+
+Friends also make life more fun. We can play games together, go on adventures, and laugh a lot. Even just hanging out and talking can be really enjoyable with a good friend.
+
+It’s important to be a good friend too. We should be kind, reliable, and treat our friends how we would want to be treated. Respect their feelings and beliefs, and appreciate the things they do for us.
+
+So remember, friends help us when we need it, make life more fun, and can make us feel a lot better when we’re feeling down. It’s important to be a good friend and to cherish the friendships we have.`;
