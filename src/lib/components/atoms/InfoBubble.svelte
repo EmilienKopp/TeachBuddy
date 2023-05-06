5afcd460e69359f542fe9ef67e7ba92496c30352
emilien <emilien@feliseed.co.jp>
2023-05-07 03:17:50 +0900

    Created info tooltip component

---
+++ src/lib/components/atoms/InfoBubble.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { Popover } from "flowbite-svelte";
+    
+    export let message: string | undefined = undefined;
+</script>
+
+<p class="px-2 ">
+    <i id="info-icon" class="bi bi-exclamation-circle-fill"></i>
+    <Popover trigger="click" triggeredBy="#info-icon">
+        💁 <span class="text-cyan-400">{message}</span>
+        <slot/>
+    </Popover>
+</p>
