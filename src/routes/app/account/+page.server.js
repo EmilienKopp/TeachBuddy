9b2ae1d8f6c4b4bcc844903956dd585b0855993f
emilien <emilien@feliseed.co.jp>
2023-05-08 02:57:51 +0900

    Made basic and language settings front/back

--- src/routes/app/account/+page.server.js
+++ src/routes/app/account/+page.server.js
@@ -1,35 +1,100 @@
 // @ts-nocheck
 
-import {
-    PUBLIC_SUPABASE_ANON_KEY,
-    PUBLIC_SUPABASE_URL
-} from '$env/static/public';
+import { languagesSettingsSchema, userBasicInfoSchema } from '$lib/config/schemas';
+import { message, superValidate } from 'sveltekit-superforms/server';
 
-import { createClient } from '@supabase/supabase-js';
-import { fail } from '@sveltejs/kit';
-import { mapHeaders } from '$lib/helpers/Arrays';
-import { superValidate } from 'sveltekit-superforms/server';
-import { vocabSettingsSchema } from '$lib/config/schemas';
-
-const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+import { toSelectOptions } from '$lib/helpers/Arrays';
+import { vertical } from '$lib/helpers/Arrays';
+import { z } from 'zod';
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load() {
-    const form = await superValidate(vocabSettingsSchema);
- 
-    const {data: grades,error} = await supabase.from('grades').select('*');
-    return { form, grades };
+export async function load({request, locals: { supabase, getSession}}) {
+    const { user } = await getSession();
+
+    const { data: studyingLanguages, error: studyingLanguagesError } = await supabase.from('studying_languages')
+                                                                                     .select('lang_code').eq('user_id', user.id);
+    const { data: nativeLanguage, error: nativeLanguageError } = await supabase.from('profiles')
+                                                                               .select('native_language')
+                                                                               .eq('id', user.id).single();
+    const userLanguages = { native_language: nativeLanguage.native_language, studying_languages: vertical(studyingLanguages,'lang_code') };
+
+    const { data: profileData, error: profileError } = await supabase.from('profiles')
+                                                                     .select('username, first_name, last_name, user_number')
+                                                                     .eq('id', user.id).single(); 
+    const { username, first_name, last_name, user_number } = profileData;
+    const userBasicInfo = { username, first_name, last_name, user_number, email: user.email };
+
+    const langForm = await superValidate(userLanguages, languagesSettingsSchema, { id: 'langForm' } );
+    const infoForm = await superValidate( userBasicInfo,userBasicInfoSchema, { id: 'infoForm' });
+
+    const session = await getSession();
+
+    let { data: grades, error: gradesError } = await supabase.from('grades').select('*');
+    let { data: languages, error: langError} = await supabase.from('languages').select('lang_code, name_native').neq('name_native',null);
+
+    grades = toSelectOptions(grades, 'id', 'name');
+    languages = toSelectOptions(languages, 'lang_code', 'name_native');
+
+    console.log(langForm, infoForm);
+
+    return { langForm, infoForm, grades, languages, session };
 }
 
 export const actions = {
-    default: async ({ request }) => {
-        const formData = await request.formData();
-        const form = await superValidate(formData, schema);
+    saveLanguageInfo: async ({ request, locals: { supabase, getSession, refreshSession } }) => {
+        const form = await superValidate(request, languagesSettingsSchema);
+        const { user } = await getSession();
+        console.log('SAVE LANGUAGE INFO server action: ', form)
+        // Validation
+        if(!form.valid) {
+            return fail(401, {form});
+        }
 
+        if(form.data.native_language) {
+            const { data: profileData, error: profileError } = await supabase.from('profiles')
+                                                    .upsert({ id: user.id, native_language: form.data.native_language })
+                                                    .eq('id', user.id).select();
+            if(profileError) console.log('Error updating profile:', profileError);
+            else console.log('Profile updated:', profileData);
+        }
 
-        return { form };
+        const selectedLanguages = form.data.studying_languages;
+        
+        const encodedLanguages = JSON.stringify(selectedLanguages).replace('[','(').replace(']',')');
+
+        if(selectedLanguages.length > 0) {
+            const studyingLanguagesRows = selectedLanguages.map(lang => ({ user_id: user.id, lang_code: lang }));
+            const { data: studyingData, error: studyingError } = await supabase.from('studying_languages')
+                                                        .upsert(studyingLanguagesRows)
+                                                        .eq('user_id', user.id);
+            // Delete where not in selectedLanguages
+            const { data: studyingDeleteData, error: studyingDeleteError } = await supabase.from('studying_languages')
+                                                        .delete()
+                                                        .eq('user_id', user.id)
+                                                        .not('lang_code', 'in', encodedLanguages);
+            if(studyingError || studyingDeleteError) console.log('Error updating studying languages:', studyingError, studyingDeleteError);
+            else console.log('Studying languages updated:', studyingData, studyingDeleteData);
+
+            const newSession = await refreshSession();
+            console.log('New session:', newSession);
+        }
+    },
+    saveBasicInfo: async ({ request, locals: { supabase, getSession, refreshSession } }) => {
+        const form = await superValidate(request, userBasicInfoSchema);
+        const { user } = await getSession();
+        // Validation
+        if(!form.valid) {
+            return fail(401, {form});
+        }
+
+        const { data: updatedProfileData, error } = 
+                                    await supabase.from('profiles')
+                                    .update({ id: user.id, 
+                                                username: form.data.username, 
+                                                first_name: form.data.first_name,
+                                                last_name: form.data.last_name, 
+                                                user_number: form.data.user_number })
+                                    .eq('id', user.id).select();
+        console.log('Updated profile data:', updatedProfileData, error);
     },
 }
-
-
-
--- src/routes/app/account/+page.svelte
+++ src/routes/app/account/+page.svelte
@@ -1,21 +1,63 @@
 <script lang="ts">
-    import UnderConstruction from "$lib/components/atoms/UnderConstruction.svelte";
-    import UserInfoForm from "$lib/components/organisms/UserInfoForm.svelte";
-    import { TabGroup, Tab } from '@skeletonlabs/skeleton';
-    import type { PageData } from "./$types";
+    import BasicInfoForm from "$lib/components/organisms/Forms/BasicInfoForm.svelte";
+    import LanguageInfoForm from "$lib/components/organisms/Forms/LanguageInfoForm.svelte";
+    import { Button, Checkbox, Chevron, Dropdown, Label, Select, Tabs, TabItem } from 'flowbite-svelte';
+    import type { SelectOptionType } from '$lib/helpers/Arrays';
+    import type { PageData } from './$types';
+    import { superForm } from 'sveltekit-superforms/client';
 
     export let data: PageData;
-    let tabSet: number = 0;
 
-    const supabase = data.supabase;
-    let userData = data.session?.user;
+    
+;
+
+    let languages: SelectOptionType[] | any[] = data.languages as SelectOptionType[] | any[];
+    let selectableLanguages: SelectOptionType[] | any[];
+    let selectedLanguages: string[] = data.session?.user.profile?.studying_languages;
+    let nativeLanguage: string = '';
     let grades = data.grades;
+
+    async function saveNativeLanguage() {
+        if(nativeLanguage) {
+            console.log('Saving native language:', nativeLanguage, data.session?.user.id);
+            const { data: profileData, error: profileError } = await data.supabase.from('profiles')
+                                                    .upsert({ id: data.session?.user.id, native_language: nativeLanguage })
+                                                    .eq('id', data.session?.user.id).select();
+            if(profileError) console.log('Error updating profile:', profileError);
+            else console.log('Profile updated:', profileData);
+        }
+
+    }
+
+    async function saveStudyingLanguages() {
+        if(selectedLanguages.length > 0) {
+            const studyingLanguagesRows = selectedLanguages.map(lang => ({ user_id: data.session?.user.id, lang_code: lang }));
+            const { data: studyingData, error: studyingError } = await data.supabase.from('studying_languages')
+                                                        .upsert(studyingLanguagesRows)
+                                                        .eq('user_id', data.session?.user.id);
+            // Delete where not in selectedLanguages
+            const { data: studyingDeleteData, error: studyingDeleteError } = await data.supabase.from('studying_languages')
+                                                        .delete()
+                                                        .eq('user_id', data.session?.user.id)
+                                                        .not('lang_code', 'in', selectedLanguages);
+            if(studyingError || studyingDeleteError) console.log('Error updating studying languages:', studyingError, studyingDeleteError);
+            else console.log('Studying languages updated:', studyingData, studyingDeleteData);
+        }
+    }
+    
+
+    $: selectableLanguages = languages.filter(lang => lang.value !== nativeLanguage);
+
 </script>
 
-<UnderConstruction />
+<div class="mt-10 md:mt-2 px-2 md:px-8">
 
-<!-- Edit User Information -->
-<!-- Path: src\routes\app\account\+page.svelte -->
-<div class="w-full">
-    <UserInfoForm bind:userData={userData} {grades}/>
-</div>
+    <Tabs>
+        <TabItem title="基本・Basic" open>
+            <BasicInfoForm {data} {grades} />
+        </TabItem>
+        <TabItem title="言語・Languages">
+            <LanguageInfoForm {data} />
+        </TabItem>
+    </Tabs>
+</div> 
