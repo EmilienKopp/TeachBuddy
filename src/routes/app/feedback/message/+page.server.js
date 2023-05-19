d6d316d38f510cb470f3ab0cb60ba866c150ec66
emilien <emilien@feliseed.co.jp>
2023-05-20 02:56:37 +0900

    added a feedback message page

---
+++ src/routes/app/feedback/message/+page.server.js
@@ -0,0 +1,46 @@
+// @ts-nocheck
+
+import { fail } from '@sveltejs/kit';
+import { superValidate } from 'sveltekit-superforms/server';
+import { z } from 'zod';
+
+const messageSchema = z.object({
+    user_id: z.string().optional(),
+    content: z.string().min(2).max(2000),
+    email: z.string().email().min(2).max(50).optional(),
+    title: z.string().min(2).max(100),
+});
+
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({locals: { supabase, getSession}}) {
+
+    const { user } = await getSession();
+
+    const form = await superValidate(  messageSchema );
+
+    return { user, form };
+}
+
+export const actions = {
+    default: async ({ request, locals: { supabase, getSession, refreshSession } }) => {
+        const formData = await request.formData();
+
+        const form = await superValidate(formData, messageSchema);
+        
+        if(!form.valid) {
+            console.log(form);
+            return fail(401, {form});
+        }
+
+        const { user } = await getSession();
+        
+        form.data.email ||= user.email;
+        const {error} = await supabase.from('feedback').insert( [{...form.data, user_id: user.id}] );
+
+        if(error) {
+            console.error(error);
+            return fail(401, {error});
+        }
+    }
+}
---
+++ src/routes/app/feedback/message/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import ContactForm from "$lib/components/organisms/Forms/ContactForm.svelte";
+    import type { PageData } from './$types';
+    import UnderConstruction from "$lib/components/atoms/UnderConstruction.svelte";
+
+    export let data: PageData;
+
+</script>
+
+<ContactForm {data}/>
