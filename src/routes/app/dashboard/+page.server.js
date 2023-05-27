c7d451d900912c6fcfb46faac6e2306a41e1edcc
EmilienKopp <emilien.kopp@gmail.com>
2023-05-27 23:16:32 +0900

    Implemented simple dashboard, surprise me feature

--- src/routes/app/dashboard/+page.server.js
+++ src/routes/app/dashboard/+page.server.js
@@ -1,14 +1,33 @@
 // @ts-nocheck
 
+import { getLastGeneratedDate, isAllowedToGenerate } from '$lib/logic/passages';
+import { message, superValidate } from 'sveltekit-superforms/server';
+
+import { ENV } from '$env/static/private';
+import { toSelectOptions } from '$lib/helpers/Arrays';
+
 /** @type {import('./$types').PageServerLoad} */
-export async function load({ locals: { supabase, getSession }}) {
+export async function load({locals: { supabase, getSession}}) {
+
     const { user } = await getSession();
 
-    let { data: profile, error: profilesError } = await supabase.from('profiles')
-                                                                .select('*')
-                                                                .eq('id', user.id).single();
-    user.profile = profile;
+    const {count:passagesCount, error } = await supabase.from("passages").select("id", {count: 'exact', head: true}).eq("owner_id", user.id);
+    const {count:wordsCount, error:wordsError } = await supabase.from("user_vocabulary").select("id", {count: 'exact', head: true}).eq("user_id", user.id);
+    
+    // Definitely a bug... The 'count' does not return AT ALL though there is one or more rows that DO get returned with the query
+    let { data: friends, error: friendshipsError } = await supabase.from("friendships")
+                                                                        .select("friend_id", {count: 'exact'})
+                                                                        .eq("user_id", user.id)
+                                                                        .eq("approved", true);
+    if(error) {
+        console.error(error);
+    }
 
-                                                            
-    return { user };
+    const stats = {
+        passagesCount,
+        wordsCount,
+        friendsCount: friends.length
+    }
+
+    return { stats };
 }
--- src/routes/app/dashboard/+page.svelte
+++ src/routes/app/dashboard/+page.svelte
@@ -1,14 +1,50 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import NavButton from "$lib/components/atoms/NavButton.svelte";
-  import Sidebar from "$lib/components/organisms/Sidebar.svelte";
-  import { Scaffolder } from "/src/config/scaffolder";
   import type { PageData } from "./$types";
+  import type { CustomUser } from "$lib/types";
+    import { Card, GradientButton } from "flowbite-svelte";
+    import UnderConstruction from "$lib/components/atoms/UnderConstruction.svelte";
+    import { random } from "$lib/helpers/Arrays";
+    import { goto } from "$app/navigation";
 
   export let data: PageData;
-  console.log(data);
+  const user: CustomUser | undefined = data.session?.user;
+  console.log(user);
+  async function surpriseMe() {
+    const languages = user?.profile?.studying_languages ?? [];
+    const randomLanguage = languages?.length > 0 ? random(languages) : "en";
+    const allPassagesForLanguage = await data.supabase.from("passages").select("id").eq("language", randomLanguage);
+    const randomPassage = random(allPassagesForLanguage.data);
+    console.log(`Opening passage #${randomPassage.id} in language [${randomLanguage.toUpperCase()}]`);
+    goto(`/app/library/${randomPassage.id}?supriseMe=true`);
+  }
 </script>
 
 <div class="mt-12 flex flex-col justify-start mx-auto w-3/4 font-gloria">
-  Welcome, {data.user.profile.username ?? data.user.email}!
+  <h2 class="text-lg md:text-2xl mb-6 font-pixel">Welcome, { user?.profile?.username ?? user?.email }!</h2>
+
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-3 font-pixel">
+    <UnderConstruction/>
+    <Card>
+      <h5 class="mb-2 text-xl md:text-3xl font-bold text-gray-900 dark:text-white">Learning・学習</h5>
+      <ul class="text-sm">
+        <li>Number of passages: 
+          <span class="font-bold">{ data.stats?.passagesCount }</span>
+        </li>
+        <li>Number of words: 
+          <span class="font-bold">{ data.stats?.wordsCount }</span>
+        </li>
+      </ul>
+    </Card>
+    <Card>
+      <h5 class="mb-2 text-xl md:text-3xl font-bold text-gray-900 dark:text-white">Friends・友達</h5>
+      <ul class="text-sm">
+        <li>Number of friends: 
+          <span class="font-bold">{ data.stats?.friendsCount ?? 0 }</span>
+        </li>
+      </ul>
+    </Card>
+    <GradientButton shadow class="col-span-1 md:col-span-2" color="redToYellow" on:click={surpriseMe}>
+      Surprise Me!
+    </GradientButton>
+  </div>
 </div>
