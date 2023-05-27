4a75e221fe9ba0b06be6fe20ceb659184d70abd8
EmilienKopp <emilien.kopp@gmail.com>
2023-05-28 01:25:41 +0900

    Helper component for readability

---
+++ src/lib/components/atoms/Underlay.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let color: string = 'white';
+    export let opacity: number | string = '50';
+    export let extraClasses: string = '';
+</script>
+
+<div class="bg-{color} bg-opacity-{opacity} p-2 rounded {extraClasses}">
+    <slot />
+</div>
