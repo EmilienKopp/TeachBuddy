c99e5f8f47080996c084b49a6ed1ae64133aee78
EmilienKopp <emilien.kopp@gmail.com>
2023-05-27 23:15:19 +0900

    Updated types

---
+++ src/lib/types.ts
@@ -0,0 +1,18 @@
+import type { User } from '@supabase/supabase-js';
+
+export interface CustomUser extends User {
+    profile?: {
+        id: string | undefined | null;
+        username?: string | undefined | null;
+        avatar_url?: string | undefined | null;
+        user_number?: string | undefined | null;
+        studying_languages?: string[] | undefined | null;
+        point_balance?: number | undefined | null;
+    } | undefined | null;
+}
+
+export type GenerationPermission = {
+    ok: boolean;
+    error?: "not_enough_points" | "too_soon" | "trial_expired" | "unknown" ;
+    messages?: string[];
+}
