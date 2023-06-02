12ff1053c3e2fcb5ecb337d9d02bd72e64beda00
EmilienKopp <emilien.kopp@gmail.com>
2023-06-03 03:05:37 +0900

    Fixed management of unfilled profiles

--- src/lib/components/organisms/Forms/BasicInfoForm.svelte
+++ src/lib/components/organisms/Forms/BasicInfoForm.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { Button, Spinner, TabItem, Tabs, Label, Input } from 'flowbite-svelte';
+    import { Button, Helper, Spinner, TabItem, Tabs, Label, Input } from 'flowbite-svelte';
     import { toSelectOptions } from '$lib/helpers/Arrays';
     import { superForm } from 'sveltekit-superforms/client';
     import SaveButton from '$lib/components/atoms/SaveButton.svelte';
@@ -7,7 +7,7 @@
     export let data: any;
     export let grades: any;
 
-    const { form: infoForm, enhance: infoFormEnhance , tainted} = superForm(data.infoForm, {
+    const { form: infoForm, enhance: infoFormEnhance , errors, tainted} = superForm(data.infoForm, {
         dataType: 'json',
         applyAction: true,
         resetForm: false,
@@ -15,6 +15,7 @@
     }); 
 
     grades = toSelectOptions(grades, 'id', 'name');
+
 </script>
 
 
@@ -22,15 +23,19 @@
     <section class="flex flex-col gap-4">
         <Label for="username">Username
             <Input type="text" id="username" name="username" bind:value={$infoForm.username} />
+            <Helper color="red">{$errors?.username ?? ''}</Helper>
         </Label>
         <Label for="first_name">First Name
             <Input type="text" id="first_name" name="first_name" bind:value={$infoForm.first_name} />
+            <Helper color="red">{$errors?.first_name ?? ''}</Helper>
         </Label>
         <Label for="last_name">Last Name
             <Input type="text" id="last_name" name="last_name" bind:value={$infoForm.last_name} />
+            <Helper color="red">{$errors?.last_name ?? ''}</Helper>
         </Label>
         <Label for="user_number">User Number / Student Number
             <Input type="text" id="user_number" name="user_number" bind:value={$infoForm.user_number} />
+            <Helper color="red">{$errors?.user_number ?? ''}</Helper>
         </Label>
     </section>
 
