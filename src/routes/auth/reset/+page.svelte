665af1794aaa592188784a1034ed257fc683fff7
EmilienKopp <emilien.kopp@gmail.com>
2023-06-03 03:09:08 +0900

    Fixed visuals

--- src/routes/auth/reset/+page.svelte
+++ src/routes/auth/reset/+page.svelte
@@ -2,15 +2,13 @@
     import { superForm } from "sveltekit-superforms/client";
     import type { PageData } from "./$types";
     import {  ButtonGroup, GradientButton, Helper, InputAddon, Input, Label, Select, Spinner} from "flowbite-svelte";
-    import {FORMS} from "$lib/config/forms";
-    import { resetSchema } from "$lib/config/schemas";
-    import SuperDebug from "sveltekit-superforms/client/SuperDebug.svelte";
+    import { resetSchema } from "/src/config/schemas";
     import Underlay from "$lib/components/atoms/Underlay.svelte";
 
     export let data: PageData;
 
     let loading: boolean = false;
-    let showPassword: boolean = false;
+    let showPassword: boolean, showPassword2: boolean = false;
     let emailError: any, passwordError: any, passwordConfirmError: any;
 
     const supabase = data.supabase;
@@ -71,15 +69,15 @@
                     <Label for="show-password2" class="md:text-xl tracking-wide">Confirm・確認
                         <ButtonGroup class="w-full mt-0">
                         <InputAddon>
-                            <button type="button" on:click={() => (showPassword = !showPassword)} tabindex="-1">
-                                {#if showPassword}
+                            <button type="button" on:click={() => (showPassword2 = !showPassword2)} tabindex="-1">
+                                {#if showPassword2}
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" /><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /></svg>
                                 {:else}
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" /></svg>
                                 {/if}
                             </button>
                             </InputAddon>
-                            <Input id="show-password2" bind:value={$form.password_confirm} type={showPassword ? 'text' : 'password'} name="password_confirm" placeholder="Your password here" {...$constraints.password_confirm}/>
+                            <Input id="show-password2" bind:value={$form.password_confirm} type={showPassword2 ? 'text' : 'password'} name="password_confirm" placeholder="Your password here" {...$constraints.password_confirm}/>
                             
                         </ButtonGroup>
                     </Label>
@@ -90,7 +88,7 @@
                     {/if}
                     <div class="mt-4 text-center">
                         <GradientButton type="submit" shadow color="lime" size="xl" class="md:text-xl w-full" on:click={ () => { loading = true }}>
-                            {FORMS.Buttons.register.label}
+                                Reset　・　リセット
                             {#if loading}
                             <Spinner class="ml-2" size="3" color="white"/>
                             {/if}
